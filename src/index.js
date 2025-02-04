cd8526f521c8710a6fc6b4c8fca75f365415bd42
Aditya Jethvanth <jethvanth.aditya4536@gmail.com>
2025-02-04 23:10:17 +0530

    Added config folder

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const {PORT} = require('./Config/serverconfig')
 const setandstartserver = async () => {
   const app = express();
-
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({extended:true})); 
   app.listen(PORT, () => {
+
+
     console.log(`Server is running on port ${PORT}`);
   });
   
