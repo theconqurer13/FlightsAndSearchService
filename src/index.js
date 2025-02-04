de856eb2121a0a419cf2ec0e834f03809c115357
Aditya Jethvanth <jethvanth.aditya4536@gmail.com>
2025-02-05 02:01:08 +0530

    crated the city model

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {PORT} = require('./Config/serverconfig')
+const {PORT} = require('../Config/serverconfig')
 const setandstartserver = async () => {
   const app = express();
   app.use(bodyParser.json());
