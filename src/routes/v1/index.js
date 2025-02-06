5a523df2a487dc461451d6d33462fb7c0b1b184d
Aditya Jethvanth <jethvanth.aditya4536@gmail.com>
2025-02-06 14:12:35 +0530

    ..

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,11 +1,6 @@
 const express = require('express');
-
 const CityController = require('../../controllers/city-controller');
-
-
 const router = express.Router();
-
 router.post('/city', CityController.create);
- 
-
+router.delete('/city/:id',CityController.destroy);  
 module.exports= router;
