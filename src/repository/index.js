5d56a539e4e2868334ff75b0caca16afafb75c89
Aditya Jethvanth <jethvanth.aditya4536@gmail.com>
2025-04-09 20:33:39 +0530

    completed flights and airplane API

--- src/repository/index.js
+++ src/repository/index.js
@@ -1,7 +1,9 @@
 
+
 module.exports={
     CityRepository:require('./City-repo'),
     AirportRepository:require('./airport-repo'),
-    CrudRespository:require('./CRUD-repo')
-    
+    CrudRespository:require('./CRUD-repo'),
+    AirplaneRepository:require('./airplane-repo'),
+    FlightRepository:require('./flights-repo'),
 }
--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,9 @@
 
 
+
 module.exports={
     CityService:require("./city-service"),
     AirportService:require("./airport-service"),
-    CrudService: require("./CRUD-service")
+    CrudService: require("./CRUD-service"),
+    FlightService:require("./flight-service")
 }
